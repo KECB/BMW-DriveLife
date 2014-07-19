28d6b13cf97bcd81180936591477339fc8ffefcb
Baiyang <KECB@BaiyangdeMacBook-Air.local>
2014-07-19 11:45:54 +0800

    Modify PoiDetailPage to get data from server

--- app/PageController.php
+++ app/PageController.php
@@ -180,7 +180,7 @@ function getPoiDetailPage($userid)
     $resultArray = end($jsondecode);
     $countArray = count($resultArray);
     $poiUid = '';
-    for( $i = 0; $i < $countArray; $i++)
+    for( $i = 0; $i < 1; $i++)
     {
         $temp = each($resultArray);
         $itemDataArray = $temp['value'];
